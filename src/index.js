483e262375f71ac31003cbefd3ff77d38be7519b
gashpen <constantinent@mail.ru>
2023-09-12 15:25:03 +0300

    homework

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/style.css';
+import './js/app.js';
+
+
+// Точка входа webpack
+// Не пишите код в данном файле
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }
+          },
+        ],
+       type: 'javascript/auto'
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  mode: 'development'
+}
